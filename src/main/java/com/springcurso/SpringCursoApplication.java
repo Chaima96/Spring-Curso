10380be0be33bee97797aa3800227fc68c74a5ca
Lucas Chaima <lucaschaima96@gmail.com>
2019-02-15 10:25:11 -0800

    Initial commit

---
+++ src/main/java/com/springcurso/SpringCursoApplication.java
@@ -0,0 +1,17 @@
+package com.springcurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCursoApplication {
+	
+	int i = 0;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCursoApplication.class, args);
+		
+	}
+
+}
+
