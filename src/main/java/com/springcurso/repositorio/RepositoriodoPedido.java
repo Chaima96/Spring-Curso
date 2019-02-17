ddbdd2b2a3fa31c594398477b94e896d8f162730
Lucas Chaima <lucaschaima96@gmail.com>
2019-02-16 18:39:40 -0800

    Criacao do Repositorio Estado_Pedido

---
+++ src/main/java/com/springcurso/repositorio/RepositoriodoPedido.java
@@ -0,0 +1,21 @@
+package com.springcurso.repositorio;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.springcurso.domain.Estado_Pedido;
+import com.springcurso.domain.Pedido;
+
+@Repository
+public interface RepositoriodoPedido extends JpaRepository<Pedido, Long> {
+	
+	public List<Pedido> findAllUsuarioId(Long id);
+	
+	@Query("UPDATE pedido SET estado = ?2 WHERE id = ?1")
+	public Pedido ActualizarEstado(Long id, Estado_Pedido estado);
+
+
+}
